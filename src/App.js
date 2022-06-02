aadc86d0e96663b8e0cba9b839f1595f298ca5b5
Snehil <snehilkr21@gmail.com>
2022-06-02 15:02:47 +0530

    lottie animation using web-lottie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import lottie from "lottie-web";
+import reactLogo from './Animation/first.json'
+
+
+//  function App() {
+//   React.useEffect(() => {
+//     lottie.loadAnimation({
+//       container: document.querySelector("#react-logo"),
+//       animationData: reactLogo
+//     });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <div id="react-logo" style={{ width: 200, height: 200 }} />
+//     </div>
+//   );
+// }
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  componentDidMount(){
+    lottie.loadAnimation({
+            container: document.querySelector("#react-logo"),
+            animationData: reactLogo
+          });
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello Snehil</h1>
+        <div id="react-logo" style={{alignContent:"center", width: 200, height: 200 }} />
+      </div>
+    )
+  }
 }
-
 export default App;
